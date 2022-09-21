bee78e2883e6fc5a2d2a54ea9a04f703f1e6c6b4
shilpykrisna <113418765+shilpykrisna@users.noreply.github.com>
2022-09-21 05:59:28 +0530

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -1,1 +1,31 @@
+phrase = "Shilpy Chaudhary"
 
+print(phrase.lower())
+
+print(phrase.upper())
+
+print(phrase.isupper())
+
+print(phrase.islower())
+
+print(phrase.upper().isupper())
+
+print(len(phrase))
+
+print(phrase[0])
+
+print(2.474862)
+
+print(-36.39743)
+
+print(3+4)
+
+print(32*3)
+
+print(44/11)
+
+print(3*4 +5)
+
+print(3*(4+5))
+
+print(10%3)
