9791e0ad717f83bcedbfa9bc368d5029b63611bc
shilpykrisna <113418765+shilpykrisna@users.noreply.github.com>
2022-09-22 17:17:28 +0530

    Create array2.py

---
+++ array2.py
@@ -0,0 +1,14 @@
+friends= ["kebin" , "jim" , "carry" , "oscar" , "toby"]
+lucky_numbers = [35,12,39,90,87]
+friends2 = friends.copy()
+
+print(friends2)
+
+print(friends.index("jim"))
+print(friends.count("jim"))
+
+friends.sort()
+print(friends)
+ 
+lucky_numbers.reverse()
+print(lucky_numbers)
