72c281ccaad82d7490972c6c7aa89b02d51e2b94
shilpykrisna <113418765+shilpykrisna@users.noreply.github.com>
2022-09-22 17:14:44 +0530

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,21 @@
+from math import *
+
+my_num =5
+print(str(my_num) + " is my favourite num")
+
+my_num1 = -5
+print(abs(my_num1))
+
+print(pow(3,2))
+
+print(max(4,6))
+
+print(min(3,6))
+
+print(round(3.4))
+
+print(floor(7.4))
+
+print(ceil(4.8))
+
+print(sqrt(36))
